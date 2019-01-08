4803fe30c5ba17e5d968dafe639a94d2c8b5e599
Camilo Villamizar <cvillami@unb.ca>
2019-01-08 16:11:26 -0400

    NBBIB-22 Duplicate on-build migration module as custom for map/debug

---
+++ custom/modules/instance_initial_content/src/Event/ReferenceMigrateEvent.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\instance_initial_content\Event;
+
+use CommerceGuys\Addressing\Country\CountryRepository;
+use Drupal\migrate_plus\Event\MigrateEvents;
+use Drupal\migrate_plus\Event\MigratePrepareRowEvent;
+use Drupal\taxonomy\Entity\Term;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Defines the migrate event subscriber.
+ */
+class ReferenceMigrateEvent implements EventSubscriberInterface {
+
+  const MIGRATION_ID = '0_instance_initial_content_references';
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[MigrateEvents::PREPARE_ROW][] = ['onPrepareRow', 0];
+    return $events;
+  }
+
+  /**
+   * React to a new row.
+   *
+   * @param \Drupal\migrate_plus\Event\MigratePrepareRowEvent $event
+   *   The prepare-row event.
+   */
+  public function onPrepareRow(MigratePrepareRowEvent $event) {
+    $row = $event->getRow();
+    $migration = $event->getMigration();
+    $migration_id = $migration->id();
+
+    // Only act on rows for this migration.
+    if ($migration_id == self::MIGRATION_ID) {
+      TRUE;
+    }
+  }
+
+}

e8fef03a2f9ab224284a3758e39669fe3e1659d4
jsanford <jsanford@unb.ca>
2019-06-07 12:54:05 -0300

    DOCKW-5 Add redis

--- build/settings/base.settings.php
+++ build/settings/base.settings.php
@@ -1,27 +1,20 @@
 <?php
-/*
-Include all settings overrides here
-# require_once 'settings_file.inc';
-*/
 
-// Load environment based includes.
+/**
+ * @file
+ * Include global settings overrides here.
+ */
+
+// Environment based includes.
 if (isset($_SERVER['APPLICATION_ENV'])) {
   $environment = strtolower($_SERVER['APPLICATION_ENV']);
   $environment_include = dirname(__FILE__) . "/settings.$environment.inc";
   if (file_exists($environment_include)) {
-    include_once $environment_include;
+    require_once $environment_include;
   }
 }
 
-// Redis Config.
-if (isset($conf['chq_redis_cache_enabled']) && $conf['chq_redis_cache_enabled']) {
-  $settings['cache']['default'] = 'cache.backend.redis';
-  $settings['redis.connection']['interface'] = 'PhpRedis';
-  $settings['redis.connection']['host'] = 'drupal-redis';
-  $settings['redis.connection']['port'] = '6379';
-  // Note that unlike memcached, redis persists cache items to disk so we can
-  // actually store cache_class_cache_form in the default cache.
-  $conf['cache_class_cache'] = 'Redis_Cache';
-}
-
-// Add common includes below.
+// Redis.
+$settings['cache_prefix']['default'] = 'nbbib_';
+$conf['chq_redis_cache_enabled'] = TRUE;
+require_once dirname(__FILE__) . "/settings.redis.inc";

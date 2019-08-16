d73b2f8b9360a1cfc77cc5338a4e8dc2efc6cd31
Camilo Villamizar <cvillami@unb.ca>
2019-08-16 15:53:40 -0300

    NBBIB-130 Implement form state restrictions and attach JS

--- custom/modules/nbbib_core/js/contrib-alpha-pager.js
+++ custom/modules/nbbib_core/js/contrib-alpha-pager.js
@@ -1,6 +1,7 @@
 (function($) {
   'use strict';
 
+  // Adds Bootstrap theme compatible classes for active list items in alpha pager.
   Drupal.behaviors.contribAlphaPager = {
     attach: function (context, settings) {
       $(document).ready( function() {
@@ -9,7 +10,7 @@
         $(".view-display-id-attachment_1 li").addClass(
           "pager__item");
         $(".view-display-id-attachment_1 li").has("a.is-active").addClass(
-          "is-active active");        
+          "is-active active");
       });
     },
   };
---
+++ custom/modules/nbbib_core/js/contrib-ief.js
@@ -0,0 +1,12 @@
+(function($) {
+  'use strict';
+
+  // Formatting and validation tools for contribs inline entity form.
+  Drupal.behaviors.contribIEF = {
+    attach: function (context, settings) {
+      $("button").click( function() {
+        alert('test');
+      });
+    },
+  };
+})(jQuery);

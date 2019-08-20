9b060cab3c7fdf308febf97a4c341a2e6a144b47
Camilo Villamizar <cvillami@unb.ca>
2019-08-20 15:22:44 -0300

    NBBIB-126 [New Contributor] button + modal dialog (doesn't modal atm)

--- custom/modules/nbbib_core/js/modal-forms.js
+++ custom/modules/nbbib_core/js/modal-forms.js
@@ -1,1 +1,11 @@
+(function($) {
+  'use strict';
 
+  // Adds Bootstrap theme compatible classes for active list items in alpha pager.
+  Drupal.behaviors.contribAlphaPager = {
+    attach: function (context, settings) {
+      $(document).ready( function() {
+      });
+    },
+  };
+})(jQuery);

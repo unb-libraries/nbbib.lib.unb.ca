40bb8764917c64034005ce6a1ce66224db4659c5
camilocodes <camilocodes@gmail.com>
2020-06-26 14:34:55 -0300

    NBBIB-195 Add logic to parse Mi'kmaq + variations as language

--- custom/modules/instance_initial_content/src/Event/ReferenceMigrateEvent.php
+++ custom/modules/instance_initial_content/src/Event/ReferenceMigrateEvent.php
@@ -77,6 +77,11 @@ class ReferenceMigrateEvent implements EventSubscriberInterface {
         if (strstr($language, 'french') || strstr('french', $language)) {
           $language = 'fre';
         }
+        elseif (strstr($language, "mi'kmaq") || strstr("mi'kmaq", $language) ||
+                strstr($language, "mi'kmaw") || strstr("mi'kmaw", $language) ||
+                strstr($language, "micmac") || strstr("micmac", $language)) {
+          $language = 'mic';
+        }
         else {
           $language = 'eng';
         }

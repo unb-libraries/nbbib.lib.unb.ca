b4ad4f88591e91e3adb86a9e146e4cb48f14d55b
camilocodes <cvillami@unb.ca>
2021-10-27 16:11:58 -0300

    NBBIB-215 Update restricted routes to check for master admin

--- custom/modules/nbbib_core/src/Routing/RouteSubscriber.php
+++ custom/modules/nbbib_core/src/Routing/RouteSubscriber.php
@@ -25,7 +25,7 @@ class RouteSubscriber extends RouteSubscriberBase {
     // Deny access to non-admins.
     foreach ($deny_routes as $deny_route) {
       if ($route = $collection->get($deny_route)) {
-        $route->setRequirement('_role', 'administrator');
+        $route->setRequirement('_uid', '1');
       }
     }
   }

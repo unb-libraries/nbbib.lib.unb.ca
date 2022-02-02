38a04b1fa376797137ccd0a92e19c30d989e0244
Camilo Villamizar <cvillami@unb.ca>
2022-02-02 14:56:03 -0400

    NBBIB-225 Pagination-formatted contributor initial filter + reset

--- custom/modules/nbbib_core/js/contrib-alpha-pager.js
+++ custom/modules/nbbib_core/js/contrib-alpha-pager.js
@@ -5,10 +5,29 @@
   Drupal.behaviors.contribAlphaPager = {
     attach: function (context, settings) {
       $(document).ready( function() {
+
+        // Create extra list item to display reset contributor list.
+        var resetLi = $("<li></li>");
+
+        $("<a>", {
+          text: "All",
+          href: "/contributors/all"
+        }).appendTo(resetLi);
+
+        // Prepend new reset item.
+        $(".view-display-id-attachment_1 ul").prepend(resetLi);
+
+        // Configure classes to macth bootstrap4 pagination.
+        $(".view-display-id-attachment_1 .item-list").addClass(
+          "pager");
+        $(".view-display-id-attachment_1 .item-list").removeClass(
+          "item-list");
         $(".view-display-id-attachment_1 ul").addClass(
           "pagination js-pager__items");
         $(".view-display-id-attachment_1 li").addClass(
-          "pager__item");
+          "page-item");
+        $(".view-display-id-attachment_1 li a").addClass(
+          "page-link");
         $(".view-display-id-attachment_1 li").has("a.is-active").addClass(
           "is-active active");
       });

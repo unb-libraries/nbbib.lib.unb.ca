142faba878c53ccad9e6a9261d90eac68c5b5f45
camilocodes <cvillami@unb.ca>
2022-02-09 13:21:01 -0400

    NBBIB-228 Add verified migration baptist + resort + update exports

--- custom/modules/instance_initial_content/src/NbBibMigrationTrait.php
+++ custom/modules/instance_initial_content/src/NbBibMigrationTrait.php
@@ -15,18 +15,26 @@ trait NbBibMigrationTrait {
    */
   public function getMigrations() {
     return [
+      'nbbib_0_new_brunswickana_1_journal_articles' => 'New Brunswickana',
+      'nbbib_0_new_brunswickana_2_books' => 'New Brunswickana',
+      'nbbib_0_new_brunswickana_3_book_sections' => 'New Brunswickana',
+      'nbbib_0_new_brunswickana_4_theses' => 'New Brunswickana',
       'nbbib_1_religion_anglican_1_journal_articles' => 'Religion: Anglican',
       'nbbib_1_religion_anglican_2_books' => 'Religion: Anglican',
       'nbbib_1_religion_anglican_3_book_sections' => 'Religion: Anglican',
       'nbbib_1_religion_anglican_4_theses' => 'Religion: Anglican',
-      'nbbib_2_new_brunswickana_1_journal_articles' => 'New Brunswickana',
-      'nbbib_2_new_brunswickana_2_books' => 'New Brunswickana',
-      'nbbib_2_new_brunswickana_3_book_sections' => 'New Brunswickana',
-      'nbbib_2_new_brunswickana_4_theses' => 'New Brunswickana',
+      'nbbib_2_religion_baptist_1_journal_articles' => 'Religion: Baptist',
+      'nbbib_2_religion_baptist_2_books' => 'Religion: Baptist',
+      'nbbib_2_religion_baptist_3_book_sections' => 'Religion: Baptist',
+      'nbbib_2_religion_baptist_4_theses' => 'Religion: Baptist',
       'nbbib_3_religion_buddhist_1_journal_articles' => 'Religion: Buddhist',
       'nbbib_3_religion_buddhist_2_books' => 'Religion: Buddhist',
       'nbbib_3_religion_buddhist_3_book_sections' => 'Religion: Buddhist',
       'nbbib_3_religion_buddhist_4_theses' => 'Religion: Buddhist',
+      'nbbib_4_religion_mormon_1_journal_articles' => 'Religion: Mormon',
+      'nbbib_4_religion_mormon_2_books' => 'Religion: Mormon',
+      'nbbib_4_religion_mormon_3_book_sections' => 'Religion: Mormon',
+      'nbbib_4_religion_mormon_4_theses' => 'Religion: Mormon',
     ];
   }
 

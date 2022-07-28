fdf9775aa9c4a8103b053b2c15e6b32f1acc39f8
camilocodes <cvillami@unb.ca>
2022-07-28 11:18:18 -0300

    NBBIB-254 Working orphan contrib cleanup (safe)

--- custom/modules/nbbib_core/src/Plugin/QueueWorker/ContribCleanup.php
+++ custom/modules/nbbib_core/src/Plugin/QueueWorker/ContribCleanup.php
@@ -20,14 +20,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class ContribCleanup extends QueueWorkerBase implements ContainerFactoryPluginInterface {
   /**
-   * For service dependency injection. Access to entity management utilities.
-   *
-   * @var Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
-
-  /**
-   * For service dependency injection. Access to logger.
+   * The logger service.
    *
    * @var Drupal\Core\Logger\LoggerChannelFactory
    */
@@ -36,10 +29,8 @@ class ContribCleanup extends QueueWorkerBase implements ContainerFactoryPluginIn
   /**
    * Constructor for ContribCleanup.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, LoggerChannelFactory $logger) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, LoggerChannelFactory $logger) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
-
-    $this->entityTypeManager = $entity_type_manager;
     $this->logger = $logger->get('nbbib_core');
   }
 
@@ -51,38 +42,22 @@ class ContribCleanup extends QueueWorkerBase implements ContainerFactoryPluginIn
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('entity_type.manager'),
       $container->get('logger.factory')
     );
-  }
-
-  /**
-   * Delete contributor if not present in any references.
-   *
-   * @param int $cid
-   *   The id of the contributor being processed.
-   */
-  public function cleanupContrib($cid) {
-    // Query for contributor in contributor->reference paragraphs.
-    $matches = $this->typeManager->getStorage('paragraph')
-      ->getQuery()
-      ->condition('type', 'yabrm_bibliographic_contributor')
-      ->condition('field_yabrm_contributor_person', $cid)
-      ->execute();
-
-    // If no matches, load contrib, delete, and log.
-    if (empty($matches)) {
-      $contrib = BibliographicContributor::load($cid);
-      $contrib->delete();
-      $this->logger->notice("Deleted orphan contributor [$cid]");
-    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function processItem($data) {
-    parent::processItem($data);
+    // Delete orphan contrib and log.
+    $cid = $data->cid;
+    $contrib = BibliographicContributor::load($cid);
+
+    if ($contrib) {
+      $contrib->delete();
+      $this->logger->notice("Cleanup: Deleted orphan contributor [$cid]");
+    }
   }
 
 }

9dd39e9e52c3716f5f84ebc048eb614bfa4d950b
Camilo Villamizar <cvillami@unb.ca>
2022-08-09 13:13:06 -0300

    NBBIB-260 Clear data items in order to avoid null references

--- custom/modules/cv_util/rm_yabrm.php
+++ custom/modules/cv_util/rm_yabrm.php
@@ -5,14 +5,14 @@
  * Contains rm-contribs.php.
  */
 
-rm_entities('taxonomy_term');
-rm_entities('paragraph');
-rm_entities('yabrm_collection');
 rm_entities('yabrm_book');
 rm_entities('yabrm_book_section');
 rm_entities('yabrm_journal_article');
 rm_entities('yabrm_thesis');
+rm_entities('yabrm_collection');
+rm_entities('paragraph');
 rm_entities('yabrm_contributor');
+rm_entities('taxonomy_term');
 
 /**
  * {@inheritdoc}

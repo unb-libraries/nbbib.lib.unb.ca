f425327e363bc0384fdd7a93df650f6d7e588bcc
Camilo Villamizar <cvillami@unb.ca>
2024-03-01 13:28:02 -0400

    NBBIB-353 Remove config-based customizable Essays header

---
+++ custom/modules/cv_util/merge_archives/merge_archives.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @file
+ * Contains merge_archives.php
+ */
+
+$archives[] = [
+  [
+    'target' => 1352,
+    'merged' => [
+      636,
+    ],
+  ],
+  [
+    'target' => 1314,
+    'merged' => [
+      761,
+      765,
+    ],
+  ],
+  [
+    'target' => 862,
+    'merged' => [
+      57,
+    ],
+  ],
+  [
+    'target' => 824,
+    'merged' => [
+      1332,
+      1348,
+    ],
+  ],
+];

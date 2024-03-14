1ad36249a3c27b1a51fa166f89c8a651e1953e7e
Camilo Villamizar <cvillami@unb.ca>
2024-03-14 15:46:38 -0300

    NBBIB-343 Publish status fix + working narrowing facets

--- custom/modules/cv_util/rm_yabrm.php
+++ custom/modules/cv_util/rm_yabrm.php
@@ -1,11 +1,6 @@
 <?php
 
-/**
- * @file
- * Contains rm_yabrm.php.
- */
-
-rm_entities('yabrm_book');
+crm_entities('yabrm_book');
 rm_entities('yabrm_book_section');
 rm_entities('yabrm_journal_article');
 rm_entities('yabrm_thesis');
@@ -14,10 +9,7 @@ rm_entities('paragraph');
 rm_entities('yabrm_contributor');
 rm_entities('taxonomy_term');
 
-/**
- * {@inheritdoc}
- */
-function rm_entities($type) {
+unction rm_entities($type) {
   $handler = \Drupal::entityTypeManager()->getStorage($type);
   $entities = $handler->loadMultiple(\Drupal::entityQuery($type)->accessCheck(FALSE)->execute());
   $handler->delete($entities);

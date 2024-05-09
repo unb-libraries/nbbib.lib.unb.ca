e29a216c800f23b88c18409928ca13f445feb6f1
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2024-05-09 14:38:57 -0300

    NBBIB-376 Single instance 'all contributors' glossary link

--- custom/modules/nbbib_core/js/contrib-alpha-pager.js
+++ custom/modules/nbbib_core/js/contrib-alpha-pager.js
@@ -1,47 +1,42 @@
 (function($) {
   'use strict';
-
   // Adds Bootstrap theme compatible classes for active list items in alpha pager.
-  Drupal.behaviors.contribAlphaPager = {
-    attach: function (context, settings) {
-      $(document).ready( function() {
-        // Preserve is-active class in menu when alpha facet is engaged.
-        var url = window.location.href;
+  $(document).ready( function() {
+    // Preserve is-active class in menu when alpha facet is engaged.
+    var url = window.location.href;
 
-        // Add is-active to "Contributors" if URL includes "/Contributors/".
-        if (url.includes("/contributors/")) {
-          $(".region-nav-main .nav-item[title='Contributors']").addClass('is-active');
-        }
+    // Add is-active to "Contributors" if URL includes "/Contributors/".
+    if (url.includes("/contributors/")) {
+      $(".region-nav-main .nav-item[title='Contributors']").addClass('is-active');
+    }
 
-        // Create extra list item to display reset contributor list.
-        var resetLi = $("<li></li>");
+    // Create extra list item to display reset contributor list.
+    var resetLi = $("<li></li>");
 
-        if (!$(".view-display-id-attachment_1 li a.is-active").length) {
-          resetLi.addClass("is-active active");
-        }
+    if (!$(".view-display-id-attachment_1 li a.is-active").length) {
+      resetLi.addClass("is-active active");
+    }
 
-        $("<a>", {
-          text: "All",
-          href: "/contributors/all",
-        }).appendTo(resetLi);
+    $("<a>", {
+      text: "All",
+      href: "/contributors/all",
+    }).appendTo(resetLi);
 
-        // Prepend new reset item.
-        $(".view-display-id-attachment_1 ul").prepend(resetLi);
+    // Prepend new reset item.
+    $(".view-display-id-attachment_1 ul").prepend(resetLi);
 
-        // Configure classes to macth bootstrap4 pagination.
-        $(".view-display-id-attachment_1 .item-list").addClass(
-          "pager");
-        $(".view-display-id-attachment_1 .item-list").removeClass(
-          "item-list");
-        $(".view-display-id-attachment_1 ul").addClass(
-          "pagination js-pager__items");
-        $(".view-display-id-attachment_1 li").addClass(
-          "page-item");
-        $(".view-display-id-attachment_1 li a").addClass(
-          "page-link");
-        $(".view-display-id-attachment_1 li").has("a.is-active").addClass(
-          "is-active active");
-      });
-    },
-  };
+    // Configure classes to macth bootstrap4 pagination.
+    $(".view-display-id-attachment_1 .item-list").addClass(
+      "pager");
+    $(".view-display-id-attachment_1 .item-list").removeClass(
+      "item-list");
+    $(".view-display-id-attachment_1 ul").addClass(
+      "pagination js-pager__items");
+    $(".view-display-id-attachment_1 li").addClass(
+      "page-item");
+    $(".view-display-id-attachment_1 li a").addClass(
+      "page-link");
+    $(".view-display-id-attachment_1 li").has("a.is-active").addClass(
+      "is-active active");
+  });
 })(jQuery);

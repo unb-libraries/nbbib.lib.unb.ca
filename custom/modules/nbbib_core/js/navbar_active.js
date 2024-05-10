3a9a0069b5c5401b70c566359d503717c1e01308
Camilo Villamizar <cvillami@unb.ca>
2024-05-10 11:06:23 -0300

    NBBIB-390 Correct JS attachment for anonymous users

--- custom/modules/nbbib_core/js/navbar_active.js
+++ custom/modules/nbbib_core/js/navbar_active.js
@@ -1,5 +1,9 @@
+/**
+ * @file
+ * Makes menu items active if the current URL starts with a matching path.
+ */
 (function($) {
-  $(document).ready(function(){
+  $(document).ready(function (e) {
     const PATH = $(location).attr('pathname');
     if (PATH != '/') {
       $(`#navbarSupportedContent a[href^="${PATH}"]`).addClass('is-active');

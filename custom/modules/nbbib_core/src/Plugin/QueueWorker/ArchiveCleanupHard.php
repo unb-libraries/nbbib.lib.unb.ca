a21fe8ffd11c770acdbe396c764e121ec1f95bc3
Camilo Villamizar <cvillami@unb.ca>
2024-05-14 11:22:26 -0300

    NBBIB-400 Queue workers for hard archive/topic cleanup (destructive)

---
+++ custom/modules/nbbib_core/src/Plugin/QueueWorker/ArchiveCleanupHard.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\nbbib_core\Plugin\QueueWorker;
+
+use Drupal\Core\Logger\LoggerChannelFactory;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\taxonomy\Entity\Term;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Deletes a bibliography archive if no references name them.
+ *
+ * @QueueWorker(
+ *   id = "archive_cleanup_hard",
+ *   title = @Translation("Orphan archive cleanup (DELETE)."),
+ *   cron = {"time" = 60}
+ * )
+ */
+class ArchiveCleanup extends QueueWorkerBase implements ContainerFactoryPluginInterface {
+  /**
+   * The logger service.
+   *
+   * @var Drupal\Core\Logger\LoggerChannelFactory
+   */
+  protected $logger;
+
+  /**
+   * Constructor for ArchiveCleanup.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, LoggerChannelFactory $logger) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->logger = $logger->get('nbbib_core');
+  }
+
+  /**
+   * Creates a new ArchiveCleanup object.
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('logger.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processItem($data) {
+    // Delete orphan term and log.
+    $tid = $data->tid;
+    $archive = Term::load($tid);
+
+    if ($archive) {
+      $archive->set('status', FALSE);
+      $archive->save();
+      $this->logger->notice("Cleanup: Unpublished orphan archive [$tid].");
+    }
+  }
+
+}
---
+++ custom/modules/nbbib_core/src/Plugin/QueueWorker/TopicCleanupHard.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\nbbib_core\Plugin\QueueWorker;
+
+use Drupal\Core\Logger\LoggerChannelFactory;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\taxonomy\Entity\Term;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Deletes a bibliography topic if no references name them.
+ *
+ * @QueueWorker(
+ *   id = "topic_cleanup_hard",
+ *   title = @Translation("Orphan topics cleanup (DELETE)."),
+ *   cron = {"time" = 60}
+ * )
+ */
+class TopicCleanup extends QueueWorkerBase implements ContainerFactoryPluginInterface {
+  /**
+   * The logger service.
+   *
+   * @var Drupal\Core\Logger\LoggerChannelFactory
+   */
+  protected $logger;
+
+  /**
+   * Constructor for TopicCleanup.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, LoggerChannelFactory $logger) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->logger = $logger->get('nbbib_core');
+  }
+
+  /**
+   * Creates a new TopicCleanup object.
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('logger.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processItem($data) {
+    // Delete orphan term and log.
+    $tid = $data->tid;
+    $topic = Term::load($tid);
+
+    if ($topic) {
+      $topic->set('status', FALSE);
+      $topic->save();
+      $this->logger->notice("Cleanup: Unpublished orphan topic [$tid].");
+    }
+  }
+
+}

3d3a870d95b26816ee27893199f97e3e108dedfd
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2024-05-31 11:31:41 -0300

    NBBIB-414 Slider update re: hover pause & restart 'stutter'

--- custom/themes/bs5_nbbib_lib_unb_ca/src/js/slideshow.js
+++ custom/themes/bs5_nbbib_lib_unb_ca/src/js/slideshow.js
@@ -2,11 +2,11 @@
   $(document).ready(function() {
     var owl = $('.owl-carousel');
     owl.owlCarousel({
-      items: 6,
       loop: true,
       autoplay: true,
       autoplayTimeout: 3000,
       autoplaySpeed: 3000,
+      autoplayHoverPause: true,
       margin: 10,
       slideTransition: 'linear',
       dots: false,
@@ -19,16 +19,16 @@
           items:3
         },
         992: {
-          items:5
+          items:6
         }
       }
     });
-    $('#block-bs5-nbbib-lib-unb-ca-views-block-nbbib-slideshow-block-1').on('mouseover', function() {
+    /*$('#block-bs5-nbbib-lib-unb-ca-views-block-nbbib-slideshow-block-1').on('mouseover', function() {
       owl.trigger('stop.owl.autoplay');
-    });  
+    });
     $('#block-bs5-nbbib-lib-unb-ca-views-block-nbbib-slideshow-block-1').on('mouseout', function() {
       owl.trigger('play.owl.autoplay');
-    });  
+    });*/
   });
 })(jQuery);
   

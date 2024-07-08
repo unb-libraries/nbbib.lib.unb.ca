bf03b22b6bac95cd47f612189272b533fb8c085e
Camilo Villamizar <cvillami@unb.ca>
2024-07-08 14:59:14 -0300

    NBBIB-451 Implement parametric MARC value extraction function

--- custom/modules/unblib_marc/script/import_marc.php
+++ custom/modules/unblib_marc/script/import_marc.php
@@ -1,10 +1,19 @@
 <?php
 
 use Scriptotek\Marc\Collection;
+use Scriptotek\Marc\Record;
 
 $source = 'modules/custom/unblib_marc/data/portolan.mrc';
 $collection = Collection::fromFile($source);
 
 foreach ($collection as $record) {
-  echo $record->title . "\n";
+  echo getMarcValue($record, '245', 'a') . "\n";
+}
+
+function getMarcValue(Record $record, string $field, string $subfield) {
+  if ($field_data = $record->getField($field)) {
+    return $field_data->getSubfield($subfield)->getData();
+  }
+
+  return NULL;
 }

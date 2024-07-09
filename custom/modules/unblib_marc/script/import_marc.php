7880bd4c7a938eac9836370b23926980f69774b8
Camilo Villamizar <cvillami@unb.ca>
2024-07-09 13:52:28 -0300

    NBBIB-451 Add trailing initial exception to trim + map

--- custom/modules/unblib_marc/script/import_marc.php
+++ custom/modules/unblib_marc/script/import_marc.php
@@ -6,17 +6,41 @@ use Scriptotek\Marc\Record;
 $source = 'modules/custom/unblib_marc/data/portolan.mrc';
 $collection = Collection::fromFile($source);
 
+$map = [
+  'item_type' => [
+    'value' => 'book',
+  ],
+  'external_key_ref' => [
+    'marc' => '001',
+  ],
+  'title' => [
+    'marc' => '245$a',
+  ],
+];
+
 foreach ($collection as $record) {
-  echo getMarcValue($record, '245', 'a', TRUE) . "\n";
+  echo getMarcValue($record, '100', 'a', TRUE) . "\n";
 }
 
-function getMarcValue(Record $record, string $field, string $subfield, bool $clean = FALSE) {
-  if ($field_data = $record->getField($field)) {
-    $data = $field_data->getSubfield($subfield)->getData();
-    $data = (is_string($data) and $clean) ? preg_replace('(^[^\p{L}\p{N}]+|[^\p{L}\p{N}]+$)u', '', $data) : $data;
-    
+function getMarcValue(Record $record, string $field, string $subfield, bool $cleanup = FALSE) {
+  if ($field_data = $record->getField($field)) { 
+
+    if ($subfield) {
+      $data = $field_data->getSubfield($subfield)->getData();
+    }
+    else {
+      $data = $field_data->toRaw();
+    }
+
+    $data = (is_string($data) and $cleanup) ? preg_replace('(^[^\p{L}\p{N}]+|[^\p{L}\p{N}]+$)u', '', $data) : $data;
+    $data = (substr($data, -2, 1) == ' ') ? "$data." : $data;
+
     return $data;
   }
-
+  
   return NULL;
 }
+
+$arg1 = $extra[0];
+
+echo "\n$arg1\n";

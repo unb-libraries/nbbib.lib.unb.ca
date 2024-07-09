718e941e37fc86bcee8d7ec3887d5b5a97c598ac
Camilo Villamizar <cvillami@unb.ca>
2024-07-09 15:59:39 -0300

    NBBIB-451 Complete NBBIB book mapping + remove portolan.csv

--- custom/modules/unblib_marc/script/import_marc.php
+++ custom/modules/unblib_marc/script/import_marc.php
@@ -11,28 +11,88 @@ $map = [
     'value' => 'book',
   ],
   'external_key_ref' => [
-    'marc' => '001',
+    'marc' => '015',
+    'marc_fallback' => '020',
+    'default' => 'NO_EXTERNAL_KEY'
   ],
   'title' => [
     'marc' => '245$a',
   ],
+  'abstract_note' => [
+    'marc' => '520$a',
+  ],
+  'publication_date' => [
+    'marc' => '260$a',
+    'process' => 'date2dmy',
+  ],
+  'short_title' => [
+    'marc' => '246$a',
+  ],
+  'language' => [
+    'marc' => '041$a',
+    'process' => 'marc2lang',
+  ],
+  'rights' => [
+    'marc' => '540$a',
+  ],
+  'archive' => [
+    'marc' => '850$a',
+  ],
+  'archive_location' => [
+    'marc' => '852$a',
+  ],
+  'library_catalog' => [
+    'marc' => '040$a',
+  ],  
+  'call_number' => [
+    'marc' => '050$a',
+  ],
+  'notes' => [
+    'marc' => '500$a',
+  ],
+  'isbn' => [
+    'marc' => '020$a',
+  ],
+  'volume' => [
+    'marc' => '490$v',
+  ],
+  'series' => [
+    'marc' => '490$a',
+  ],
+  'publisher' => [
+    'marc' => '260$b',
+  ],
+  'place' => [
+    'marc' => '260$a',
+  ],
+  'edition' => [
+    'marc' => '250$a',
+  ],
+  'physical_description' => [
+    'marc' => '300',
+  ],
 ];
 
 foreach ($collection as $record) {
-  echo getMarcValue($record, '100', 'a', TRUE) . "\n";
+  echo getMarcValue($record, '300', '', TRUE) . "\n";
 }
 
 function getMarcValue(Record $record, string $field, string $subfield, bool $cleanup = FALSE) {
-  if ($field_data = $record->getField($field)) { 
-
-    if ($subfield) {
+  // If there is field data...
+  if ($field_data = $record->getField($field)) {
+    // If a subfield is requested and valid...
+    if ($subfield and gettype($field_data->getSubfield($subfield)) == 'object') {
+      // Get data.
       $data = $field_data->getSubfield($subfield)->getData();
     }
     else {
+      // Get raw broad field data.
       $data = $field_data->toRaw();
     }
-
-    $data = (is_string($data) and $cleanup) ? preg_replace('(^[^\p{L}\p{N}]+|[^\p{L}\p{N}]+$)u', '', $data) : $data;
+    // If cleanup is requested, trim spaces and special characters.
+    $data = (is_string($data) and $cleanup) ? 
+      preg_replace('(^[^\p{L}\p{N}]+|[^\p{L}\p{N}]+$)u', '', $data) : $data;
+    // Restore trailing period if last character appears to be an initial.
     $data = (substr($data, -2, 1) == ' ') ? "$data." : $data;
 
     return $data;

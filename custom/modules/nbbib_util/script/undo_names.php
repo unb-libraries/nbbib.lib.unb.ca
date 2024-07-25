72aa6095093f1ad4058118bc9e6d513a320398f3
Camilo Villamizar <cvillami@unb.ca>
2024-07-25 14:33:06 -0300

    NBBIB-451 Add progress indicator

--- custom/modules/nbbib_util/script/undo_names.php
+++ custom/modules/nbbib_util/script/undo_names.php
@@ -9,14 +9,11 @@ function upd_entities($type) {
     $first_name = trim($entity->getFirstName());
     $last_name = trim($entity->getLastName());
     $institution_name = $entity->getInstitutionName() ? trim($entity->getInstitutionName()) : NULL;
-    if ($institution_name) {
-        $entity->setName("$institution_name");
+    if (!$institution_name) {
+      $entity->setName("$first_name $last_name");
+      $entity->save();
+      echo "\nUpdated contributor [$first_name $last_name]";
     }
-    else {
-        $entity->setName("$first_name $last_name");
-    }
-
-    $entity->save();
   }  
-  echo "All entities of type [$type] updated.\n";
+  echo "\nAll entities of type [$type] updated.\n";
 }

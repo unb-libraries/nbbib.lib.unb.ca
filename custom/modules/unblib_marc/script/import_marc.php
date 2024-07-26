1e4f312dc5e80f1d07066fda8ca5287a449d6a39
Camilo Villamizar <cvillami@unb.ca>
2024-07-26 16:15:52 -0300

    NBBIB-451 Refactor code for multivalues + extract + custom separator

--- custom/modules/unblib_marc/script/import_marc.php
+++ custom/modules/unblib_marc/script/import_marc.php
@@ -2,6 +2,7 @@
 
 use Drupal\yabrm\Entity\BookReference;
 use Scriptotek\Marc\Collection;
+use Scriptotek\Marc\Fields\Field;
 use Scriptotek\Marc\Record;
 
 $source = 'modules/custom/unblib_marc/data/portolan.mrc';
@@ -34,7 +35,7 @@ $map = [
   ],
   'contributors' => [
     'marc' => '700',
-    'multivalue' => TRUE,
+    'multival' => TRUE,
     'process' => 'create_contribs',
   ],
   'short_title' => [
@@ -127,7 +128,7 @@ function migrateMarc(string $source, string $entity_type, array $map) {
         
       if ($marc_field) {
         $value = getMarcValue($record, $marc_field, $marc_subfield, $cleanup, $multival);
-        echo "\n|FIELD|$field: $$value";
+        echo "\n|FIELD|$field: $value";
       }
       elseif (isset($mapping['default'])) {
         $value = $mapping['default'];
@@ -146,7 +147,7 @@ function migrateMarc(string $source, string $entity_type, array $map) {
       }
       
       if ($value) {
-        $entity->set($field, $value);
+        //$entity->set($field, $value);
       }
     }
     $n++;
@@ -168,29 +169,36 @@ function getMarcValue(
   bool $cleanup = FALSE,
   bool $multival = FALSE
   ) {
-  // If there is field data...
-  $field_data = $multival ? $record->query($field) : $record->getField($marc_field);
-  if ($field_data) {
-    // If a subfield is requested and valid...
-    if ($marc_subfield and gettype($field_data->getSubfield($marc_subfield)) == 'object') {
-      // Get data.
-      $data = $field_data->getSubfield($marc_subfield)->getData();
-    }
-    else {
-      // Get raw broad field data.
-      $data = $multival ? $data : $field_data->toRaw();
-    }
-    // If cleanup is requested, trim spaces and special characters.
-    $data = (is_string($data) and $cleanup) ? 
-      preg_replace('(^[^\p{L}\p{N}]+|[^\p{L}\p{N}]+$)u', '', $data) : $data;
-    // Restore trailing period if last character appears to be an initial.
-    $data = (substr($data, -2, 1) == ' ') ? "$data." : $data;
+  // If multivalue, run query. If single value, get field.
+  $field_data = $multival ? $record->query($marc_field) : [$record->getField($marc_field)];
+  $data = "";
+  $entries = 0;
 
-    return $data;
+  foreach ($field_data as $entry) {
+    if ($entry) {
+      if ($marc_subfield and gettype($entry->getSubfield($marc_subfield)) == 'object') {
+        // Get entry data.
+        $entry_data = $entry->getSubfield($marc_subfield)->getData();
+      }
+      else {
+        // Get raw, broad field data.
+        $entry_data = $entry->toRaw();
+      }
+
+      // If cleanup is requested, trim spaces and special characters.
+      $entry_data = (is_string($entry_data) and $cleanup) ? 
+      preg_replace('(^[^\p{L}\p{N}]+|[^\p{L}\p{N}]+$)u', '', $entry_data) : $entry_data;
+      // Restore trailing period if last character appears to be an initial.
+      $entry_data = (substr($entry_data, -2, 1) == ' ') ? "$entry_data." : $entry_data;
+      // Concatenate data string.
+      $data .= $entries > 0 ? "|$entry_data" : $entry_data;
+      $entries++;
+    }
   }
-  
-  return NULL;
+
+  return $data;
 }
+
 
 function date2dmy($field, $date) {
   preg_match('~\b\d{4}\b\+?~', $date, $year);

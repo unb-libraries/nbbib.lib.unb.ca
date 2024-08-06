96dee3fec56d5575ad5ac87a4f63f48eac366728
Camilo Villamizar <cvillami@unb.ca>
2024-08-06 16:28:36 -0300

    NBBIB-451 Methods to create + de-dupe contributors/terms on import

--- custom/modules/unblib_marc/script/import_marc.php
+++ custom/modules/unblib_marc/script/import_marc.php
@@ -1,6 +1,8 @@
 <?php
 
-use Drupal\yabrm\Entity\BookReference;
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\taxonomy\Entity\Term;
+use Drupal\yabrm\Entity\BibliographicContributor;
 use Scriptotek\Marc\Collection;
 use Scriptotek\Marc\Fields\Field;
 use Scriptotek\Marc\Record;
@@ -30,7 +32,7 @@ $map = [
   ],
   'author' => [
     'marc' => '100$a',
-    'process' => 'create_contribs',
+    'process' => 'create_author',
     'cleanup' => TRUE,
   ],
   'contributors' => [
@@ -128,7 +130,6 @@ function migrateMarc(string $source, string $entity_type, array $map) {
         
       if ($marc_field) {
         $value = getMarcValue($record, $marc_field, $marc_subfield, $cleanup, $multival);
-        echo "\n|FIELD|$field: $value";
       }
       elseif (isset($mapping['default'])) {
         $value = $mapping['default'];
@@ -137,14 +138,19 @@ function migrateMarc(string $source, string $entity_type, array $map) {
         $value = $mapping['value'];
       }
 
+      echo "\n|FIELD|$field: $value";
+      $reached = 'NO';
+
       if(isset($mapping['process'])) {
         $callback = $mapping['process'];
         if (is_callable($callback)) {
           if (function_exists($callback)) {
-            $value = $callback($field, $value);
+            $value = $callback($value, $entity);
+            $reached = 'YES';
           }
         }
       }
+      echo "\n|FIELD_POST|$field: $value|CALLBACK: $reached";
       
       if ($value) {
         //$entity->set($field, $value);
@@ -199,8 +205,7 @@ function getMarcValue(
   return $data;
 }
 
-
-function date2dmy($field, $date) {
+function date2dmy($date, &$entity) {
   preg_match('~\b\d{4}\b\+?~', $date, $year);
 
   if (isset($year[0])) {
@@ -210,11 +215,179 @@ function date2dmy($field, $date) {
   return;
 }
 
-function create_contribs($field, $contribs) {
+function create_author($contrib_name, &$entity) {
+  return $entity->getTitle();
+}
+
+function create_contribs($contrib_blob, &$entity) {
+  return;
+}
+
+/**
+ * Create contributor paragraph entities.
+ *
+ * @param array $contrib_names
+ *   Array containing the names of contributors to add.
+ * @param string $contrib_role
+ *   The contributor role to apply.
+ *
+ * @return Drupal\paragraphs\Entity\Paragraph[]
+ *   The created paragraph entities
+ *
+ * @throws Drupal\Core\Entity\EntityStorageException
+ */
+function createContributors($contrib_names, $contrib_role) {
+  // Contributors.
+  $contrib_ids = [];
+
+  // Create contribs.
+  foreach ($contrib_names as $contrib_name) {
+    // Trim whitespace and remove periods.
+    $contrib_name = trim($contrib_name, " \t\n\r\0\x0B\x2E");
+    // Keep exact trimmed copy to prevent duplicate import.
+    $zotero_name = $contrib_name;
+    // If contributor is anonymous...
+    if (strpos(mb_strtolower($contrib_name), 'anonymous')) {
+      $zotero_name = $contrib_name = 'Anonymous';
+    }
+
+    if (!empty($contrib_name)) {
+      $existing = $this->typeManager->getStorage('yabrm_contributor')
+        ->getQuery()
+        ->condition('zotero_name', $zotero_name)
+        ->accessCheck(FALSE)
+        ->execute();
+
+      reset($existing);
+      $contrib_id = key($existing);
+
+      // Create contributor if doesn't exist.
+      if (empty($contrib_id)) {
+        $institution_name = NULL;
+        $first_name = NULL;
+        $last_name = NULL;
+        $sort_name = NULL;
+
+        $split_name = explode(',', $contrib_name);
+
+        if ($split_name[0] == $contrib_name) {
+          $institution_name = $contrib_name;
+          $sort_name = $institution_name;
+        }
+        else {
+          $last_name = trim($split_name[0]);
+          $first_name = trim(implode(',', array_slice($split_name, 1)));
+          $sort_name = $last_name;
+        }
+
+        $contrib = BibliographicContributor::create([
+          'zotero_name' => $zotero_name,
+          'institution_name' => $institution_name,
+          'first_name' => $first_name,
+          'last_name' => $last_name,
+          'sort_name' => $sort_name,
+          'status' => FALSE,
+        ]);
+
+        $contrib->save();
+        $contrib_id = $contrib->id();
+      }
+
+      // Populate array with contributor ids.
+      $contrib_ids[] = $contrib_id;
+    }
+  }
+
+  // Create contrib Paragraph references using ids.
+  $contributors = [];
+  $voc = 'yabrm_contributor_roles';
+  $field = 'name';
+
+  foreach ($contrib_ids as $contrib_id) {
+    // Retrieve contributor role ID, or create term.
+    $role_term = ucwords(str_replace('_', ' ', $contrib_role));
+    $role_tid = $this->taxTermExists($role_term, $field, $voc);
+
+    if (!$role_tid) {
+      $term = Term::create([
+        'vid' => $voc,
+        $field => $role_term,
+      ]);
+      $term->save();
+
+      $role_tid = $term->id();
+    }
+
+    // Create Paragraph.
+    $values = [
+      [
+        'field' => 'field_yabrm_contributor_person',
+        'value' => $contrib_id,
+      ],
+      [
+        'field' => 'field_yabrm_contributor_role',
+        'value' => $role_tid,
+      ],
+    ];
+
+    $contributors[] = $this->createParagraph('yabrm_bibliographic_contributor', $values);
+  }
+
+  return $contributors;
+}
+
+/**
+ * Check if a taxonomy term exists.
+ *
+ * @param string $value
+ *   The name of the term.
+ * @param string $field
+ *   The field to match when validating.
+ * @param string $vocabulary
+ *   The vid to match.
+ *
+ * @return mixed
+ *   Contains an INT of the tid if exists, FALSE otherwise.
+ */
+function taxTermExists($value, $field, $vocabulary) {
+  $query = $this->typeManager->getStorage('taxonomy_term')->getQuery();
+  $query->condition('vid', $vocabulary);
+  $query->condition($field, $value);
+  $tids = $query->accessCheck(FALSE)->execute();
+
+  if (!empty($tids)) {
+    foreach ($tids as $tid) {
+      return $tid;
+    }
+  }
   return FALSE;
 }
 
-function full_title($field, $title) {
+/**
+ * Create a new Paragraph.
+ *
+ * @param string $type
+ *   Type of Paragraph.
+ * @param array $values
+ *   Associative array of values to be added to Paragraph.
+ *
+ * @return int
+ *   ID of created Paragraph.
+ */
+function createParagraph($type, array $values) {
+  $paragraph = Paragraph::create(['type' => $type]);
+
+  foreach ($values as $value) {
+    $paragraph->set($value['field'], $value['value']);
+  }
+
+  // Migrate all paragraphs as unpublished.
+  $paragraph->set('status', FALSE);
+  $paragraph->save();
+  return $paragraph;
+}
+
+function full_title($title, &$entity) {
   return $title;
 }
 

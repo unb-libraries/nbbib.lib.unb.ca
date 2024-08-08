a58d0f01a845ece2cc7ce1e142e271c7018eccf0
Camilo Villamizar <cvillami@unb.ca>
2024-08-08 13:20:35 -0300

    NBBIB-453 Citation copy JS + update nbbib_core libraries

---
+++ custom/modules/nbbib_core/js/cite_copy.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * Copies a reference's citation on button press.
+ */
+(function($) {
+  $(document).ready(function (e) {
+    // Transcription copy button.
+    $('#cite').click(function() {
+      navigator.clipboard.writeText($('#citation').html().toString().replace(/<\/?[^>]+>/gi, '')).then(
+        function() {
+          // Clipboard successfully set.
+          window.alert('Citation copied to clipboard') 
+        }, 
+        function() {
+          // Clipboard write failed.
+          window.alert('ERROR: Clipboard API unsupported by browser')
+        }
+      );
+    });
+  });
+})(jQuery);

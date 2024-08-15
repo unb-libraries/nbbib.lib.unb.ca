7f92ae7584cf3eeaf13f8f3e0d4f82b6eed0a82a
Camilo Villamizar <cvillami@unb.ca>
2024-08-15 19:00:59 -0300

    NBBIB-453 Fixed in-window messages + citation spacing

--- custom/modules/nbbib_core/js/cite_copy.js
+++ custom/modules/nbbib_core/js/cite_copy.js
@@ -8,16 +8,22 @@
     // Transcription copy button.
     $('#cite').click(function() {
       // Copy html, convert to string,remove tags, replace breaks with spaces.
-      navigator.clipboard.writeText($('.views-field-bibliographic-citation > .field-content').html().toString().replace(/<\/?[^>]+>/gi, '').replace(/\n/g, " ")).then(
+      navigator.clipboard.writeText($('.views-field-bibliographic-citation > .field-content')
+      .html()
+      .toString()
+      .replace(/<\/?[^>]+>/gi, '')
+      .replace(/\n/g, ' ')
+      .replace(/\s+/g, ' ')
+      .trim()).then(
         function() {
           // Clipboard successfully set.
-          window.alert('Citation copied to clipboard') 
+          $('#cite-success').removeClass('visually-hidden');
         }, 
         function() {
           // Clipboard write failed.
-          window.alert('ERROR: Clipboard API unsupported by browser')
+          $('#cite-error').removeClass('visually-hidden');
         }
-      );
+  );
     });
     $('.ui-dialog-titlebar-close').click(function(e) {
       // Prevent default and reload on window close to reset.
@@ -26,29 +32,3 @@
     });
   });
 })(jQuery);
-
-/*
-(function ($, Drupal) {
-  Drupal.behaviors.citeCopy = {
-    attach: function(context) {
-
-      $(window).on('dialog:aftercreate', function(dialog, $element, settings) {
-        // Transcription copy button.
-        $('#cite').click(function() {
-          // Copy html, convert to string,remove tags, replace breaks with spaces.
-          navigator.clipboard.writeText($('.views-field-bibliographic-citation > .field-content').html().toString().replace(/<\/?[^>]+>/gi, '').replace(/\n/g, " ")).then(
-            function() {
-              // Clipboard successfully set.
-              $('#cite-success').removeClass('d-none');
-            }, 
-            function() {
-              // Clipboard write failed.
-              $('#cite-error').removeClass('d-none');
-            }
-          );
-        });
-      });
-    }
-  };
-})(jQuery, Drupal);
-*/

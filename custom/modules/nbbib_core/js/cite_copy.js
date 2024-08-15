848924d73b769121621b4994acc20610add6b53a
Camilo Villamizar <cvillami@unb.ca>
2024-08-15 16:51:39 -0300

    Revert "NBBIB-453 In-window success/fail notification instead of pop-up (test)"
    This reverts commit 0f0a262083695dda88ebc737459f4277d082570d.

--- custom/modules/nbbib_core/js/cite_copy.js
+++ custom/modules/nbbib_core/js/cite_copy.js
@@ -11,13 +11,13 @@
       navigator.clipboard.writeText($('.views-field-bibliographic-citation > .field-content').html().toString().replace(/<\/?[^>]+>/gi, '').replace(/\n/g, " ")).then(
         function() {
           // Clipboard successfully set.
-          $('#cite-success').removeClass('d-none');
+          window.alert('Citation copied to clipboard') 
         }, 
         function() {
           // Clipboard write failed.
-          $('#cite-error').removeClass('d-none');
+          window.alert('ERROR: Clipboard API unsupported by browser')
         }
-  );
+      );
     });
     $('.ui-dialog-titlebar-close').click(function(e) {
       // Prevent default and reload on window close to reset.

95f37b42aae78682580afdce146b3c4ad6540432
Camilo Villamizar <cvillami@unb.ca>
2024-08-15 13:45:15 -0300

    NBBIB-453 Use modal open event to attach copy btn behaviour

--- custom/modules/nbbib_core/js/cite_copy.js
+++ custom/modules/nbbib_core/js/cite_copy.js
@@ -2,26 +2,31 @@
  * @file
  * Copies a reference's citation on button press.
  */
-(function($) {
-  $(document).ready(function (e) {
-    // Transcription copy button.
-    $('#cite').click(function() {
-      // Copy html, convert to string,remove tags, replace breaks with spaces.
-      navigator.clipboard.writeText($('.views-field-bibliographic-citation > .field-content').html().toString().replace(/<\/?[^>]+>/gi, '').replace(/\n/g, " ")).then(
-        function() {
-          // Clipboard successfully set.
-          window.alert('Citation copied to clipboard') 
-        }, 
-        function() {
-          // Clipboard write failed.
-          window.alert('ERROR: Clipboard API unsupported by browser')
-        }
-      );
-    });
-    $('.ui-dialog-titlebar-close').click(function(e) {
-      // Prevent default and reload on window close to reset.
-      e.preventDefault();
-      location.reload(true);
-    });
-  });
-})(jQuery);
+(function ($, Drupal) {
+  Drupal.behaviors.sampleModalReaction = {
+    attach: function(context) {
+
+      $(window).on('dialog:aftercreate', function(dialog, $element, settings) {
+        // Transcription copy button.
+        $('#cite').click(function() {
+          // Copy html, convert to string,remove tags, replace breaks with spaces.
+          navigator.clipboard.writeText($('.views-field-bibliographic-citation > .field-content').html().toString().replace(/<\/?[^>]+>/gi, '').replace(/\n/g, " ")).then(
+            function() {
+              // Clipboard successfully set.
+              window.alert('Citation copied to clipboard') 
+            }, 
+            function() {
+              // Clipboard write failed.
+              window.alert('ERROR: Clipboard API unsupported by browser')
+            }
+          );
+        });
+        $('.ui-dialog-titlebar-close').click(function(e) {
+          // Prevent default and reload on window close to reset.
+          e.preventDefault();
+          location.reload(true);
+        });
+      });
+    }
+  };
+})(jQuery, Drupal);

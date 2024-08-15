c33a48c791b11e1e1e3544037a3e3dfdb86e7362
Camilo Villamizar <cvillami@unb.ca>
2024-08-15 16:26:40 -0300

    NBBIB-453 Reimplement without Drupal behaviors

--- custom/modules/nbbib_core/js/cite_copy.js
+++ custom/modules/nbbib_core/js/cite_copy.js
@@ -2,8 +2,34 @@
  * @file
  * Copies a reference's citation on button press.
  */
+(function($) {
+  'use strict';
+  $(window).on('dialog:aftercreate', function (dialog, $element, settings) {
+    // Transcription copy button.
+    $('#cite').click(function() {
+      // Copy html, convert to string,remove tags, replace breaks with spaces.
+      navigator.clipboard.writeText($('.views-field-bibliographic-citation > .field-content').html().toString().replace(/<\/?[^>]+>/gi, '').replace(/\n/g, " ")).then(
+        function() {
+          // Clipboard successfully set.
+          window.alert('Citation copied to clipboard') 
+        }, 
+        function() {
+          // Clipboard write failed.
+          window.alert('ERROR: Clipboard API unsupported by browser')
+        }
+      );
+    });
+    $('.ui-dialog-titlebar-close').click(function(e) {
+      // Prevent default and reload on window close to reset.
+      e.preventDefault();
+      location.reload(true);
+    });
+  });
+})(jQuery);
+
+/*
 (function ($, Drupal) {
-  Drupal.behaviors.sampleModalReaction = {
+  Drupal.behaviors.citeCopy = {
     attach: function(context) {
 
       $(window).on('dialog:aftercreate', function(dialog, $element, settings) {
@@ -21,12 +47,8 @@
             }
           );
         });
-        $('.ui-dialog-titlebar-close').click(function(e) {
-          // Prevent default and reload on window close to reset.
-          e.preventDefault();
-          location.reload(true);
-        });
       });
     }
   };
 })(jQuery, Drupal);
+*/

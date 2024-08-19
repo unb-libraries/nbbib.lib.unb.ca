8ffe341f8ed37f3a98d424cd64241e756b3c93cd
Camilo Villamizar <cvillami@unb.ca>
2024-08-19 15:41:46 -0300

    NBBIB-461 Add script to recover trailing initial periods on contribs

--- custom/modules/nbbib_util/script/format_initials.php
+++ custom/modules/nbbib_util/script/format_initials.php
@@ -2,22 +2,28 @@
 
 /**
  * @file
- * Contains format_names.php.
+ * Contains format_initials.php.
  */
 
-format_names('yabrm_contributor');
+format_initials('yabrm_contributor');
 
-function format_names($type) {
+function format_initials($type) {
   $handler = \Drupal::entityTypeManager()->getStorage($type);
   $entities = $handler->loadMultiple(\Drupal::entityQuery($type)->accessCheck(FALSE)->execute());
   foreach ($entities as $entity) {
     $first_name = trim($entity->getFirstName());
     $last_name = trim($entity->getLastName());
     $institution_name = $entity->getInstitutionName() ? trim($entity->getInstitutionName()) : NULL;
+
     if (!$institution_name) {
-      $entity->setName("$last_name, $first_name");
-      $entity->save();
-      echo "\nUpdated contributor [$first_name $last_name]";
+      $lastchar = substr($first_name, -1);
+      $prevchar = substr($first_name, -2, 1);
+
+      if (ctype_alpha($lastchar) && $prevchar == ' ') {
+        echo "\nUpdating contributor [$first_name $last_name] -> [$first_name. $last_name]";
+        // $entity->setFirstName("$last_name, $first_name");
+        // $entity->save();
+      }
     }
   }  
   echo "\nAll entities of type [$type] updated.\n";

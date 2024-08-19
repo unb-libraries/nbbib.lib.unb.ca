8f79e48778cbe2da12ebdac27c03eaf090ed089d
Camilo Villamizar <cvillami@unb.ca>
2024-08-19 14:28:02 -0300

    NBBIB-453 Reimplement JS as Drupal behav + button href (restores tab)

--- custom/modules/nbbib_core/js/cite_copy.js
+++ custom/modules/nbbib_core/js/cite_copy.js
@@ -2,28 +2,36 @@
  * @file
  * Copies a reference's citation on button press.
  */
-(function($) {
+(function($, navigator, once) {
   'use strict';
-  $(window).on('dialog:aftercreate', function (dialog, $element, settings) {
-    // Transcription copy button.
-    $('#cite').click(function() {
-      // Copy html, convert to string,remove tags, replace breaks with spaces.
-      navigator.clipboard.writeText($('.views-field-bibliographic-citation > .field-content')
-      .html()
-      .toString()
-      .replace(/<\/?[^>]+>/gi, '')
-      .replace(/\n/g, ' ')
-      .replace(/\s+/g, ' ')
-      .trim()).then(
-        function() {
-          // Clipboard successfully set.
-          $('#cite-success').removeClass('visually-hidden');
-        }, 
-        function() {
-          // Clipboard write failed.
-          $('#cite-error').removeClass('visually-hidden');
-        }
-  );
-    });
-  });
-})(jQuery);
+
+  Drupal.behaviors.citeCopy = {
+    attach: function (context, settings) {
+      // On document ready.
+      once('citeCopy', 'html').forEach(function (element) {
+        $('html').on('dialog:aftercreate', function (dialog, element, settings) {
+          // Transcription copy button.
+          $('#cite').click(function() {
+            // Copy html, convert to string,remove tags, replace breaks with spaces.
+            navigator.clipboard.writeText($('.views-field-bibliographic-citation > .field-content')
+            .html()
+            .toString()
+            .replace(/<\/?[^>]+>/gi, '')
+            .replace(/\n/g, ' ')
+            .replace(/\s+/g, ' ')
+            .trim()).then(
+              function() {
+                // Clipboard successfully set.
+                $('#cite-success').removeClass('visually-hidden');
+              }, 
+              function() {
+                // Clipboard write failed.
+                $('#cite-error').removeClass('visually-hidden');
+              }
+            );
+          });
+        });
+      });
+    }
+  }
+}(jQuery, navigator, once));

79c7ba01a046664c07c365346dbf1cf527df9b1a
Camilo Villamizar <cvillami@unb.ca>
2024-08-30 16:29:48 -0300

    NBBIB-464 Make citation button keyboard clickable

--- custom/modules/nbbib_core/js/cite_copy.js
+++ custom/modules/nbbib_core/js/cite_copy.js
@@ -11,6 +11,26 @@
       once('citeCopy', 'html').forEach(function (element) {
         $('html').on('dialog:aftercreate', function (dialog, element, settings) {
           // Transcription copy button.
+          $('#cite').keypress(function(e) {
+            // Copy html, convert to string,remove tags, replace breaks with spaces.
+            navigator.clipboard.writeText($('#drupal-modal .views-field > .field-content')
+            .html()
+            .toString()
+            .replace(/<\/?[^>]+>/gi, '')
+            .replace(/\n/g, ' ')
+            .replace(/\s+/g, ' ')
+            .replace('&amp;', '&')
+            .trim()).then(
+              function() {
+                // Clipboard successfully set.
+                $('#cite-success').removeClass('visually-hidden');
+              }, 
+              function() {
+                // Clipboard write failed.
+                $('#cite-error').removeClass('visually-hidden');
+              }
+            );
+        });
           $('#cite').click(function() {
             // Copy html, convert to string,remove tags, replace breaks with spaces.
             navigator.clipboard.writeText($('#drupal-modal .views-field > .field-content')

c86ed632e46d0f63f637af581e3357295b903b54
Camilo Villamizar <cvillami@unb.ca>
2025-01-15 15:42:20 -0400

    NBBIB-465 Revert migration mapping for new types

--- custom/modules/nbbib_initial_content/src/NbBibMigrationTrait.php
+++ custom/modules/nbbib_initial_content/src/NbBibMigrationTrait.php
@@ -95,8 +95,8 @@ trait NbBibMigrationTrait {
   public function getZoteroTypeMappings() {
     return [
       'journalArticle' => 'yabrm_journal_article',
-      'conferencePaper' => 'yabrm_journal_article',
-      'report' => 'yabrm_journal_article',
+      'conferencePaper' => 'yabrm_conference',
+      'report' => 'yabrm_report',
       'book' => 'yabrm_book',
       'bookSection' => 'yabrm_book_section',
       'thesis' => 'yabrm_thesis',

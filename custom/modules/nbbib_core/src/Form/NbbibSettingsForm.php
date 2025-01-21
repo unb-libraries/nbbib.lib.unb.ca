407f0dfe33d718a6fbca3e718f84e553c7370c71
Camilo Villamizar <cvillami@unb.ca>
2025-01-21 16:11:57 -0400

    NBBIB-489 Add configurable list of reference types

--- custom/modules/nbbib_core/src/Form/NbbibSettingsForm.php
+++ custom/modules/nbbib_core/src/Form/NbbibSettingsForm.php
@@ -21,7 +21,7 @@ class NbbibSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function getFormId() {
-    return 'nbbib_core_admin_settings';
+    return 'nbbib_settings_form';
   }
 
   /**
@@ -38,6 +38,26 @@ class NbbibSettingsForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config(static::SETTINGS);
+    // Retrieve reference types and format as string for text field. 
+    $types = $config->get('nbbib_types');
+    $lines = [];
+
+    foreach ($types as $type) {
+      $lines[] = implode(':', array_values($type));
+    }
+
+    $types = implode("\n", array_values($lines));
+
+    $form['#title'] = $this->t('NBBIB Settings');
+
+    $form['nbbib_types'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Valid NBBIB reference types'),
+      '#description' => $this->t('Each type must be entered in a new line as entity_id:singular:plural (colon-separated, ' . 
+        'e.g. yabrm_book_sections:Book Section:Book Sections). <i><b>Caution: Malformed entries can cause site-wide issues.</b></i>'),
+      '#rows' => 12,
+      '#default_value' => $types,
+    ];
     $form['nbbib_essays_header'] = [
       '#type' => 'text_format',
       '#title' => $this->t('Custom header for Essays list page'),
@@ -51,13 +71,29 @@ class NbbibSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Transform text back into configuration array.
+    $types = $form_state->getValue('nbbib_types');
+    $lines = explode("\n", $types);
+    $types = [];
+
+    foreach ($lines as $line) {
+      $tokens = explode(':', $line);
+
+      $types[] = [
+        'entity' => $tokens[0],
+        'singular' => $tokens[1],
+        'plural' => $tokens[2],
+      ];
+    }
+
     // Retrieve the configuration.
     $this->configFactory->getEditable(static::SETTINGS)
       // Set the submitted configuration setting.
+      ->set('nbbib_types', $types)
       ->set('view_headers.nbbib_essays', $form_state->getValue('nbbib_essays_header')['value'])
       ->save();
     
-      $form_state->setRedirect('view.nbbib_essays.page_1');
+    $form_state->setRedirect('view.nbbib_essays.page_1');
     parent::submitForm($form, $form_state);
   }
 }

22f003878dc0de593f46e1e8b2b2f408c5bfa9ff
Camilo Villamizar <cvillami@unb.ca>
2025-02-13 08:37:25 -0400

    NBBIB-475 Adjust yabrm publishing script for new types

--- custom/modules/nbbib_util/script/pub_yabrm.php
+++ custom/modules/nbbib_util/script/pub_yabrm.php
@@ -5,18 +5,18 @@ $arg2 = isset($extra[1]) and ($extra[1] == 'y') ? TRUE : FALSE;
 $timestamp = strtotime($arg1);
 
 if ($timestamp) {
-  pub_entities('yabrm_book', $timestamp);
-  pub_entities('yabrm_book_section', $timestamp);
-  pub_entities('yabrm_journal_article', $timestamp);
-  pub_entities('yabrm_thesis', $timestamp);
-  pub_entities('yabrm_conference', $timestamp);
-  pub_entities('yabrm_report', $timestamp);
-  pub_entities('yabrm_collection', $timestamp);
-  pub_entities('yabrm_contributor', $timestamp);
-  pub_entities('paragraph', $timestamp);
+  pub_entities('yabrm_book', $timestamp, $arg2);
+  pub_entities('yabrm_book_section', $timestamp, $arg2);
+  pub_entities('yabrm_journal_article', $timestamp, $arg2);
+  pub_entities('yabrm_thesis', $timestamp, $arg2);
+  pub_entities('yabrm_conference', $timestamp, $arg2);
+  pub_entities('yabrm_report', $timestamp, $arg2);
+  pub_entities('yabrm_collection', $timestamp, $arg2);
+  pub_entities('yabrm_contributor', $timestamp, $arg2);
+  pub_entities('paragraph', $timestamp, $arg2);
 }
 
-function pub_entities($type, $timestamp) {
+function pub_entities($type, $timestamp, $publish) {
   $readable = date(DATE_ATOM, $timestamp);
   $handler = \Drupal::entityTypeManager()->getStorage($type);
   $entities = $handler->loadMultiple(\Drupal::entityQuery($type)
@@ -24,10 +24,10 @@ function pub_entities($type, $timestamp) {
     ->condition('created', $timestamp, '>')
     ->execute());
 
-  $update = $arg2 ? 'published' : 'unpublished';
+  $update = $publish ? 'published' : 'unpublished';
 
   foreach ($entities as $entity) {
-    $entity->setPublished($arg2)->save();
+    $entity->setPublished($publish)->save();
   }
 
   echo "All entities of type [$type] modified after [$readable] $update.\n";

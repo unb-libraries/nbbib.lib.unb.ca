8215453bb8f578a152f7fbca0568755a0c222794
Camilo Villamizar <cvillami@unb.ca>
2025-03-11 14:56:13 -0300

    NBBIB-594 Add form definition classes

---
+++ custom/modules/yabrm/src/Form/WebsiteReferenceDeleteForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\yabrm\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting Website reference entities.
+ *
+ * @ingroup yabrm
+ */
+class WebsiteReferenceDeleteForm extends ContentEntityDeleteForm {
+
+
+}
---
+++ custom/modules/yabrm/src/Form/WebsiteReferenceRevisionDeleteForm.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Drupal\yabrm\Form;
+
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Form\ConfirmFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a form for deleting a Website reference revision.
+ *
+ * @ingroup yabrm
+ */
+class WebsiteReferenceRevisionDeleteForm extends ConfirmFormBase {
+
+
+  /**
+   * The Website reference revision.
+   *
+   * @var \Drupal\yabrm\Entity\WebsiteReferenceInterface
+   */
+  protected $revision;
+
+  /**
+   * The Website reference storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $websiteReferenceStorage;
+
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $connection;
+
+  /**
+   * The date formatter service.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  protected $dateFormatter;
+
+  /**
+   * Constructs a new WebsiteReferenceRevisionDeleteForm.
+   *
+   * @param \Drupal\Core\Entity\EntityStorageInterface $entity_storage
+   *   The entity storage.
+   * @param \Drupal\Core\Database\Connection $connection
+   *   The database connection.
+   * @param \Drupal\Core\Datetime\DateFormatterInterface $date_formatter
+   *   The date formatter service.
+   */
+  public function __construct(
+    EntityStorageInterface $entity_storage,
+    Connection $connection,
+    DateFormatterInterface $date_formatter) {
+    $this->websiteReferenceStorage = $entity_storage;
+    $this->connection = $connection;
+    $this->dateFormatter = $date_formatter;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    $entity_manager = $container->get('entity_type.manager');
+    return new static(
+      $entity_manager->getStorage('yabrm_website'),
+      $container->get('database'),
+      $container->get('date.formatter')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'yabrm_website_revision_delete_confirm';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getQuestion() {
+    return $this->t('Are you sure you want to delete the revision from %revision-date?', ['%revision-date' => $this->dateFormatter->format($this->revision->getRevisionCreationTime())]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCancelUrl() {
+    return new Url('entity.yabrm_website.version_history', ['yabrm_website' => $this->revision->id()]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfirmText() {
+    return $this->t('Delete');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $yabrm_website_revision = NULL) {
+    $this->revision = $this->websiteReferenceStorage->loadRevision($yabrm_website_revision);
+    $form = parent::buildForm($form, $form_state);
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->websiteReferenceStorage->deleteRevision($this->revision->getRevisionId());
+
+    $this->logger('content')->notice('Website reference: deleted %title revision %revision.', [
+      '%title' => $this->revision->label(),
+      '%revision' => $this->revision->getRevisionId(),
+    ]);
+    $this->messenger()->addMessage($this->t('Revision from %revision-date of Website reference %title has been deleted.', [
+      '%revision-date' => $this->dateFormatter->format($this->revision->getRevisionCreationTime()),
+      '%title' => $this->revision->label(),
+    ]));
+    $form_state->setRedirect(
+      'entity.yabrm_website.canonical',
+       ['yabrm_website' => $this->revision->id()]
+    );
+    if ($this->connection->query('SELECT COUNT(DISTINCT vid) FROM {yabrm_website_revision} WHERE id = :id', [':id' => $this->revision->id()])->fetchField() > 1) {
+      $form_state->setRedirect(
+        'entity.yabrm_website.version_history',
+         ['yabrm_website' => $this->revision->id()]
+      );
+    }
+  }
+
+}

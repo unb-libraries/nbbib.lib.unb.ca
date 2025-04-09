4d80305e08d8a1cebc7d3eb3c7947bdab3d48a7f
Camilo Villamizar <cvillami@unb.ca>
2025-04-09 15:02:45 -0300

    NBBIB-615 Rewrite header collapse in pure JS and throttle scroll event

--- custom/themes/bs5_nbbib_lib_unb_ca/src/js/custom-scroll.js
+++ custom/themes/bs5_nbbib_lib_unb_ca/src/js/custom-scroll.js
@@ -1,37 +1,53 @@
-(function ($, Drupal, once) {
+(function (Drupal, once) {
   Drupal.behaviors.customScroll = {
     attach: function (context, settings) {
       var banner = document.querySelector('header[role="banner"]');
+      
+      // Throttle function to improve performance
+      function throttle(func, limit) {
+        let lastFunc;
+        let lastRan;
+        return function () {
+          const context = this;
+          const args = arguments;
+          if (!lastRan) {
+            func.apply(context, args);
+            lastRan = Date.now();
+          } else {
+            clearTimeout(lastFunc);
+            lastFunc = setTimeout(function () {
+              if ((Date.now() - lastRan) >= limit) {
+                func.apply(context, args);
+                lastRan = Date.now();
+              }
+            }, limit - (Date.now() - lastRan));
+          }
+        };
+      }
 
-      $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 0) {
-          if (!(banner.classList.contains('scrolling'))) {
+      // Scroll event handler with throttling
+      const handleScroll = throttle(function () {
+        if (window.scrollY > 0) {
+          if (!banner.classList.contains('scrolling')) {
             banner.classList.add('scrolling');
           }
-        }
-        else {
+        } else {
           banner.classList.remove('scrolling');
         }
-      });
-      $(window).on('load', function() {
+      }, 100); // Adjust the limit (100ms) as needed
+
+      // Load and resize event handler
+      const handleLoadResize = function () {
         if (window.innerWidth < 992) {
           banner.classList.remove('sticky-top');
-        }
-        else {
+        } else {
           banner.classList.add('sticky-top');
+        }
+      };
 
-        }
-      });
-      $(window).on('resize', function() {
-        if (window.innerWidth < 992) {
-          banner.classList.remove('sticky-top');
-        }
-        else {
-          banner.classList.add('sticky-top');
-
-        }
-      });
+      window.addEventListener('scroll', handleScroll);
+      window.addEventListener('load', handleLoadResize);
+      window.addEventListener('resize', handleLoadResize);
     }
   };
-})(jQuery, Drupal, once);
-  
+})(Drupal, once);

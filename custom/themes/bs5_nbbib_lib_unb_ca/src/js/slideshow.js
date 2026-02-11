ca3784206584ed7505b983bbb792092352b0cc3a
Camilo Villamizar <cvillami@unb.ca>
2026-02-11 13:57:53 -0400

    IN-382 Random slide wave effect

--- custom/themes/bs5_nbbib_lib_unb_ca/src/js/slideshow.js
+++ custom/themes/bs5_nbbib_lib_unb_ca/src/js/slideshow.js
@@ -1,31 +1,5 @@
 (function ($) {
   $(document).ready(function() {
-    // Initial carousel instancing and setup.
-    var owl = $('.owl-carousel');
-    owl.owlCarousel({
-      items: 12,
-      loop: true,
-      autoplay: true,
-      autoplayTimeout: 3000,
-      autoplaySpeed: 4000,
-      autoplayHoverPause: true,
-      slideTransition: 'linear',
-      dots: false,
-      nav: false,
-      responsive: {
-        0: {
-          items:2,
-          autoplayTimeout: 2400,
-        },
-        576: {
-          items:3,
-        },
-        992: {
-          items:6,
-          autoplayTimeout: 3000,
-        }
-      },
-    });
     // Randomize slide photo sizes.
     var mobile = window.matchMedia("(max-width: 991px)")
     var baseWidth = mobile.matches ? 50 : 60;
@@ -37,17 +11,18 @@
     // Align slides in a wave and set to fit content width.
     slides = $('.swiper-slide');
     slides.each( function(index) {
+      imgWidth = $(this).find('img').width();
       // If odd, align center.
       if (index % 2) {
-        $(this).attr('style', 'align-items: center !important; width: fit-content !important;');
+        $(this).attr('style', `align-self: center !important;`);
       }
       // If multiple of 4, align bottom.
       else if (index % 4) {
-        $(this).attr('style', 'align-items: end !important; width: fit-content !important;');
+        $(this).attr('style', `align-self: end !important;`);
       }
       // Otherwise it's an even non-multiple of 4, align top.
       else {
-        $(this).attr('style', 'align-items: start !important; width: fit-content !important');
+        $(this).attr('style', `align-self: start !important;`);
       }
     });
   });

084b28b1b3eb7637d5cb36ea87373b26e24d77d3
Camilo Villamizar <cvillami@unb.ca>
2026-05-13 15:49:57 -0300

    NBBIB-632 Remove delay

--- custom/themes/bs5_nbbib_lib_unb_ca/src/js/slideshow.js
+++ custom/themes/bs5_nbbib_lib_unb_ca/src/js/slideshow.js
@@ -1,32 +1,30 @@
 (function ($) {
   $(document).ready(function() {
-    setTimeout(() => {
-      // Randomize slide photo sizes.
-      var mobile = window.matchMedia("(max-width: 991px)")
-      var baseWidth = mobile.matches ? 46 : 56;
-      var variation = 5;
-      photos = $('.swiper-slide img');
-      photos.each( function() {
-        randomWidth = Math.floor((Math.random() * baseWidth * 1.5) + baseWidth * 2.5);
-        $(this).attr('style', `width: ${randomWidth}px !important;`);
-      });
-      // Align slides in a wave and set to fit content width.
-      slides = $('.swiper-slide');
-      slides.each( function(index) {
-        imgWidth = $(this).find('img').width();
-        // If odd, align center.
-        if (index % 2) {
-          $(this).attr('style', `align-self: center !important;`);
-        }
-        // If multiple of 4, align bottom.
-        else if (index % 4) {
-          $(this).attr('style', `align-self: end !important;`);
-        }
-        // Otherwise it's an even non-multiple of 4, align top.
-        else {
-          $(this).attr('style', `align-self: start !important;`);
-        }
-      });
-    }, 300);
+    // Randomize slide photo sizes.
+    var mobile = window.matchMedia("(max-width: 991px)")
+    var baseWidth = mobile.matches ? 46 : 56;
+    var variation = 5;
+    photos = $('.swiper-slide img');
+    photos.each( function() {
+      randomWidth = Math.floor((Math.random() * baseWidth * 1.5) + baseWidth * 2.5);
+      $(this).attr('style', `width: ${randomWidth}px !important;`);
+    });
+    // Align slides in a wave and set to fit content width.
+    slides = $('.swiper-slide');
+    slides.each( function(index) {
+      imgWidth = $(this).find('img').width();
+      // If odd, align center.
+      if (index % 2) {
+        $(this).attr('style', `align-self: center !important;`);
+      }
+      // If multiple of 4, align bottom.
+      else if (index % 4) {
+        $(this).attr('style', `align-self: end !important;`);
+      }
+      // Otherwise it's an even non-multiple of 4, align top.
+      else {
+        $(this).attr('style', `align-self: start !important;`);
+      }
+    });
   });
 })(jQuery);
